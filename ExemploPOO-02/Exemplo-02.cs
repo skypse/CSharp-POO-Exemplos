c82be57ece55cb14d72c6e1d5680656edd111315
skypse <skypse0136@gmail.com>
2024-06-20 11:46:40 -0300

    adicionando exemplo 02 com POO

---
+++ ExemploPOO-02/Exemplo-02.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPOO_02
+{
+    class Exemplo_02
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}

c7b3c027d78bcf09193a711a182df06315e8eeeb
skypse <skypse0136@gmail.com>
2024-06-20 12:52:57 -0300

    adicionando exemplo

---
+++ ExemploPOO-03/Games.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPOO_03
+{
+    internal class Games
+    {
+        public string nome = "The Sims 4";
+        public int anoLancamento = 2014;
+        public int nota = 10;
+
+    }
+}

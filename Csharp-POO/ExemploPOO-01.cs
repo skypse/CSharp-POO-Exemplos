0dfc7d4b3a33df65e758e365f135b09db2f6b341
skypse <skypse0136@gmail.com>
2024-06-20 10:01:00 -0300

    Adicionar arquivos de projeto.

---
+++ Csharp-POO/ExemploPOO-01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPOO_01
+{
+    internal class Carro
+    {
+            string modelo="Fusca 1600", fabricante ="Volkswagem", nome ="Fusca", cor ="Azul";
+            int ano = 1974;
+        public static void Main(string[] args)
+        {
+            Carro carro01 = new Carro();
+            Carro carro02 = new Carro();
+
+            Console.WriteLine(carro01.nome);
+            Console.WriteLine(carro02.nome);
+            Console.WriteLine(carro01.modelo);
+            Console.WriteLine(carro02.modelo);
+            Console.WriteLine(carro01.cor);
+            Console.WriteLine(carro02.cor);
+            Console.WriteLine(carro01.fabricante);
+            Console.WriteLine(carro02.fabricante);
+            Console.WriteLine(carro01.ano);
+            Console.WriteLine(carro02.ano);
+        }
+    }
+}
